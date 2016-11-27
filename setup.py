8c799bbf4b348bce84ea3e5c5ae1a96345f7ce4c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-11-27 21:30:17 +1100

    Increment version to 1.3.3 and change README file format.

--- setup.py
+++ setup.py
@@ -15,29 +15,18 @@ def _template_files():
                     result.append(os.path.join(root[len(prefix)+1:], name))
     return result
 
-long_description = """
-This package provides a Python library for working with the open source
-OpenShift Origin project and downstream OpenShift products from Red Hat.
-
-The library will provide the capability to work with OpenShift/Kubernetes
-resource objects, as well as endpoints for communicating with the OpenShift
-REST API.
-
-The package requires Python 3 and will not work with Python 2.
-"""
+long_description = open('README.rst').read()
 
 classifiers = [
     'Development Status :: 4 - Beta',
     'License :: OSI Approved :: BSD License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
 ]
 
 setup_kwargs = dict(
     name='powershift',
-    version='1.3.2',
+    version='1.3.3',
     description='Python library for working with OpenShift.',
     long_description=long_description,
     url='https://github.com/getwarped/powershift',
