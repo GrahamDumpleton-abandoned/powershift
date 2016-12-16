63eb3fbe2370947e2e5e37d84d6c11a695313638
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-12-16 17:37:46 +1100

    Increment to version 1.3.6.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = [
 
 setup_kwargs = dict(
     name='powershift',
-    version='1.3.5',
+    version='1.3.6',
     description='Python library for working with OpenShift.',
     long_description=long_description,
     url='https://github.com/getwarped/powershift',
