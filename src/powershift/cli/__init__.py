5cb2f97e04c12584b9cf2317eefbe54bd0520f2d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-12-19 13:29:14 +1100

    Add checks for whether oc installed and add commands for querying information about session.

--- src/powershift/cli/__init__.py
+++ src/powershift/cli/__init__.py
@@ -22,22 +22,6 @@ def server_url():
 
     return url
 
-def server_context():
-    command = ['oc', 'whoami', '-c']
-
-    context = subprocess.check_output(command, universal_newlines=True)
-    context = context.strip()
-
-    return context
-
-def server_token():
-    command = ['oc', 'whoami', '-t']
-
-    token = subprocess.check_output(command, universal_newlines=True)
-    token = token.strip()
-
-    return token
-
 @click.group()
 @click.pass_context
 def root(ctx):
@@ -53,6 +37,23 @@ def root(ctx):
         https://github.com/getwarped/powershift
 
     """
+
+    # Check whether the 'oc' command is installed and fail if not. We
+    # are so dependent on it being installed that there isn't much point
+    # continuing if it isn't.
+
+    try:
+        subprocess.check_output(['oc', 'help'], stderr=subprocess.STDOUT)
+
+    except FileNotFoundError:
+        click.echo('Failed: You do not appear to have the \'oc\' command '
+                'line tool installed. Please install it to continue.')
+        ctx.exit(1)
+
+    except subprocess.CalledProcessError:
+        click.echo('Failed: You appear to have the \'oc\' command '
+                'line tool installed, but it appears to be non functional.')
+        ctx.exit(1)
 
 @root.command()
 def console():
@@ -98,6 +99,82 @@ def bash():
     script = os.path.join(directory, 'completion-bash.sh')
     with open(script, encoding='UTF-8') as fp:
         click.echo(fp.read())
+
+def session_context():
+    command = ['oc', 'whoami', '-c']
+
+    context = subprocess.check_output(command, stderr=subprocess.STDOUT,
+            universal_newlines=True)
+    context = context.strip()
+
+    return context
+
+def session_token():
+    command = ['oc', 'whoami', '-t']
+
+    token = subprocess.check_output(command, stderr=subprocess.STDOUT,
+            universal_newlines=True)
+    token = token.strip()
+
+    return token
+
+def session_user():
+    command = ['oc', 'whoami']
+
+    token = subprocess.check_output(command, stderr=subprocess.STDOUT,
+            universal_newlines=True)
+    token = token.strip()
+
+    return token
+
+@root.group()
+def session():
+    """
+    Display information about current session.
+
+    """
+
+@session.command()
+@click.pass_context
+def user(ctx):
+    """
+    Displays name of user for active session.
+
+    """
+
+    try:
+        click.echo(session_user())
+    except subprocess.CalledProcessError as e:
+        click.echo('Failed: %s' % e.stdout)
+        ctx.exit(e.returncode)
+
+@session.command()
+@click.pass_context
+def context(ctx):
+    """
+    Displays the active user session context.
+
+    """
+
+    try:
+        click.echo(session_context())
+    except subprocess.CalledProcessError as e:
+        click.echo('Failed: %s' % e.stdout)
+        ctx.exit(e.returncode)
+
+@session.command()
+@click.pass_context
+def token(ctx):
+    """
+    Displays the active user session token.
+
+    """
+
+    try:
+        click.echo(session_token())
+    except subprocess.CalledProcessError as e:
+        click.echo('Failed: %s' % e.stdout)
+        ctx.exit(e.returncode)
 
 def main():
     # Import any plugins for extending the available commands. They
